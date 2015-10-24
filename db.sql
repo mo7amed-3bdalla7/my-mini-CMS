eb8704106c71a2e2f7df48689f0380306e4668b4
mohamed abdallah <mo7amed-3bdalla7@users.noreply.github.com>
2015-10-24 20:55:25 +0200

    Update db.sql

--- db.sql
+++ db.sql
@@ -27,15 +27,6 @@ CREATE TABLE IF NOT EXISTS `categories` (
   UNIQUE KEY `id` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
--- Dumping data for table project.categories: ~4 rows (approximately)
-/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
-INSERT INTO `categories` (`id`, `user_id`, `created`, `title`, `slug`, `content`) VALUES
-	(3, 0, '2015-06-24 14:25:20', 'web', '', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\r\nquis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo'),
-	(4, 0, '2015-06-24 14:26:33', 'Technology', '', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\r\nquis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\r\nconsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\r\ncillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\r\nproident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
-	(5, 0, '2015-06-24 14:28:16', 'Education', '', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\r\nquis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\r\nconsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\r\ncillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\r\nproident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
-	(6, 0, '2015-06-24 14:29:44', 'Business', '', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\r\nquis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\r\nconsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\r\ncillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\r\nproident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
-/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
-
 
 -- Dumping structure for table project.comments
 CREATE TABLE IF NOT EXISTS `comments` (
@@ -46,16 +37,6 @@ CREATE TABLE IF NOT EXISTS `comments` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
--- Dumping data for table project.comments: ~6 rows (approximately)
-/*!40000 ALTER TABLE `comments` DISABLE KEYS */;
-INSERT INTO `comments` (`id`, `user_id`, `content`, `created`) VALUES
-	(1, 1, 'mohamed', '0000-00-00 00:00:00'),
-	(2, 1, 'mohamed', '0000-00-00 00:00:00'),
-	(3, 1, 'mohamed', '0000-00-00 00:00:00'),
-	(4, 1, 'mohamed', '0000-00-00 00:00:00'),
-	(5, 1, 'mohamed', '0000-00-00 00:00:00'),
-	(6, 1, 'mohamed', '2015-06-17 13:32:08');
-/*!40000 ALTER TABLE `comments` ENABLE KEYS */;
 
 
 -- Dumping structure for table project.news
@@ -69,16 +50,6 @@ CREATE TABLE IF NOT EXISTS `news` (
   `tag` varchar(200) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
-
--- Dumping data for table project.news: ~5 rows (approximately)
-/*!40000 ALTER TABLE `news` DISABLE KEYS */;
-INSERT INTO `news` (`id`, `title`, `user_id`, `created`, `content`, `cat`, `tag`) VALUES
-	(1, 'new1', 3, '2015-06-24 15:15:41', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\r\nquis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\r\nconsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\r\ncillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\r\nproident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'web', ''),
-	(2, 'new2', 3, '2015-06-27 09:10:58', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\r\nquis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\r\nconsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\r\ncillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\r\nproident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'Technology', ''),
-	(3, 'new3', 3, '2015-06-27 09:11:11', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\r\nquis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\r\nconsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\r\ncillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\r\nproident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'Education', ''),
-	(4, 'new4', 3, '2015-06-27 09:11:37', 'ex ea commodo\r\nconsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\r\ncillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\r\nproident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'Business', ''),
-	(5, 'test', 3, '2015-06-27 15:00:31', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\r\nquis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\r\nconsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\r\ncillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\r\nproident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'Business', '');
-/*!40000 ALTER TABLE `news` ENABLE KEYS */;
 
 
 -- Dumping structure for table project.pages
@@ -94,12 +65,6 @@ CREATE TABLE IF NOT EXISTS `pages` (
   `publish` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
-
--- Dumping data for table project.pages: ~1 rows (approximately)
-/*!40000 ALTER TABLE `pages` DISABLE KEYS */;
-INSERT INTO `pages` (`id`, `user_id`, `category_id`, `created`, `last_modified`, `title`, `slug`, `content`, `publish`) VALUES
-	(4, 1, 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '1', '1', '', 0);
-/*!40000 ALTER TABLE `pages` ENABLE KEYS */;
 
 
 -- Dumping structure for table project.settings
@@ -125,13 +90,6 @@ CREATE TABLE IF NOT EXISTS `slideshow` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
--- Dumping data for table project.slideshow: ~1 rows (approximately)
-/*!40000 ALTER TABLE `slideshow` DISABLE KEYS */;
-INSERT INTO `slideshow` (`id`, `file_name`, `title`, `description`) VALUES
-	(1, 'test', '1', '2015');
-/*!40000 ALTER TABLE `slideshow` ENABLE KEYS */;
-
-
 -- Dumping structure for table project.users
 CREATE TABLE IF NOT EXISTS `users` (
   `id` smallint(6) NOT NULL AUTO_INCREMENT,
@@ -148,12 +106,6 @@ CREATE TABLE IF NOT EXISTS `users` (
   UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
--- Dumping data for table project.users: ~2 rows (approximately)
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` (`id`, `username`, `password`, `email`, `gender`, `status`, `activation`, `privilege`, `lastlogin`) VALUES
-	(3, 'mohamed', '374b72d68abb54769396d7fefb307e16', 'mo7amed.3bdalla7@gmail.com', 1, 1, '', 1, '2015-06-27 14:42:07'),
-	(4, 'ahmed', '18fe3de77e54faeb4760a00a1e5efab3', 'mo7amed.3bdalla7@yahoo.com', 1, 0, '6b5c10f2ca4a1eb187f7edcf1ecdc02c', 0, '0000-00-00 00:00:00');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
